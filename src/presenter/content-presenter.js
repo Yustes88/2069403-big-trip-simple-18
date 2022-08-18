e496f9d0af9b057d2923ecb9b633a4b39c1137ac
Yulya <iuliiaobr.io@gmail.com>
2022-08-18 22:20:05 +0700

    Реализует смену точки маршрута на форму

--- src/presenter/content-presenter.js
+++ src/presenter/content-presenter.js
@@ -1,6 +1,7 @@
 import { render } from '../render.js';
 import SortFormView from '../view/sort-form-view.js';
 import TripEventItemView from '../view/trip-event-item-view.js';
+import TripFormAddView from '../view/trip-form-edit.js';
 import TripList from '../view/trip-list.js';
 
 
@@ -28,6 +29,25 @@ export default class ContentPresenter {
 
   #renderPoint = (tripPoint) => {
     const tripPointComponent = new TripEventItemView(tripPoint);
+    const tripFormEditComponent = new TripFormAddView(tripPoint);
+
+    const replacePointWithForm = () => {
+      this.#tripListComponent.element.replaceChild(tripFormEditComponent.element, tripPointComponent.element);
+    };
+
+    const replaceFormWithPoint = () => {
+      this.#tripListComponent.element.replaceChild(tripPointComponent.element, tripFormEditComponent.element);
+    };
+
+    tripPointComponent.element.querySelector('.event__rollup-btn').addEventListener('click', () =>
+      replacePointWithForm());
+
+    tripFormEditComponent.element.querySelector('form').addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      replaceFormWithPoint();
+    });
+
+
     render(tripPointComponent, this.#tripListComponent.element);
   };
 
