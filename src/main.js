5c61d732aad3ba04dbdad3707d227726764c1d35
Yulya <iuliiaobr.io@gmail.com>
2022-08-22 20:17:56 +0700

    Добавит структуру данных фильтров и обучит компонент фильтров принимать данные

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { render } from './framework/render.js';
+import { generateFilter } from './mock/filters-mock.js';
 import TripsModel from './model/trip-point-model.js';
 import ContentPresenter from './presenter/content-presenter.js';
 import FiltersFormView from './view/filters-form-view.js';
@@ -9,6 +10,8 @@ const siteContentWrapperElement = document.querySelector('.trip-events');
 const tripPointModel = new TripsModel();
 const contentPresenter = new ContentPresenter(siteContentWrapperElement, tripPointModel);
 
+const filters = generateFilter(tripPointModel.points);
 
-render(new FiltersFormView(), siteFilterElement);
+
+render(new FiltersFormView(filters), siteFilterElement);
 contentPresenter.init();
--- src/utile/trip-point-utile.js
+++ src/utile/trip-point-utile.js
@@ -4,5 +4,7 @@ const humanizeDate = (date) => dayjs(date).format('DD/MM/YY H:00');
 const humanizeHour = (hour) => dayjs(hour).format(' HH:00');
 const humanizeStartDate = (startDate) => dayjs(startDate).format('MMMM DD');
 const differenceHoursMinutes = (dateFrom,dateTo) => dayjs(dateTo).diff(dayjs(dateFrom),'hour','minute');
+const isStartDateExpired = (startDate) => startDate && dayjs().isAfter(startDate, 'D MMMM');
 
-export {humanizeDate, humanizeHour, humanizeStartDate, differenceHoursMinutes};
+
+export {humanizeDate, humanizeHour, humanizeStartDate, differenceHoursMinutes, isStartDateExpired};
