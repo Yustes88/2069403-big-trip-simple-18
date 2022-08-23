4246e9752e5476c186233e572546c17a03a6a5d8
Yulya <iuliiaobr.io@gmail.com>
2022-08-23 14:35:59 +0700

    Исправляет сортировку цен на максимальное к минимальному

--- src/utile/sort-utile.js
+++ src/utile/sort-utile.js
@@ -5,7 +5,7 @@ const sort = {
   [SORT_TYPES.DATE]: (tripPoints) => tripPoints.sort((a, b) => a.dateFrom - b.dateFrom),
   [SORT_TYPES.EVENT]: (tripPoints) => tripPoints,
   [SORT_TYPES.TIME]: (tripPoints) => tripPoints,
-  [SORT_TYPES.PRICE]: (tripPoints) => tripPoints.sort((a, b) => a.basePrice - b.basePrice),
+  [SORT_TYPES.PRICE]: (tripPoints) => tripPoints.sort((a, b) => b.basePrice - a.basePrice),
   [SORT_TYPES.OFFER]: (tripPoints) => tripPoints,
 };
 
