c8d58fe5d3956e70ecccb9ebfe7323888c872b3a
Yulya <iuliiaobr.io@gmail.com>
2022-10-02 09:00:24 +0700

    Удаление неиспользуемой части кода

--- src/utile/trip-point-utile.js
+++ src/utile/trip-point-utile.js
@@ -4,12 +4,8 @@ const isEscKey = (evt) => evt.key === 'Escape' || evt.key === 'Esc';
 const humanizeDate = (date) => dayjs(date).format('DD/MM/YY H:00');
 const humanizeHour = (hour) => dayjs(hour).format(' HH:00');
 const humanizeStartDate = (startDate) => dayjs(startDate).format('MMM DD');
-const differenceHoursMinutes = (dateFrom,dateTo) => dayjs(dateTo).diff(dayjs(dateFrom),'hour','minute');
-const isDateAfter = (date) => date && dayjs().isAfter(date, 'D MMM');
-const isDateSame = (date) => date && dayjs().isSame(date, 'D MMM');
-const isDateBefore = (date) => date && dayjs().isBefore(date, 'D MMM');
 const isPriceNumber = (basePrice) => parseInt(basePrice, 10);
 const isDatesSame = (dateA, dateB) => (dateA === null && dateB === null) || dayjs(dateA).isSame(dateB, 'D');
 const isPriceSame = (pointA, pointB) => pointA.basePrice === pointB.basePrice;
 
-export {isEscKey, humanizeDate, humanizeHour, humanizeStartDate, differenceHoursMinutes, isDateAfter, isDateSame, isDateBefore, isPriceNumber, isDatesSame, isPriceSame};
+export {isEscKey, humanizeDate, humanizeHour, humanizeStartDate, isPriceNumber, isDatesSame, isPriceSame};
